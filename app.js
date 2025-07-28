3e89cb5dc9fefe50c12b473046096a218467d9bf
Abdallah Elaraby <pipo.elaraby99@gmail.com>
2025-07-28 17:48:46 +0300

    sadsadsa

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors())
 // })
 
 mongoose.connect(
-  'mongodb://ahmed:gaITI%402025@github-actions-iti-2025.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false',
+  'github-actions-iti-2025.global.mongocluster.cosmos.azure.com',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
